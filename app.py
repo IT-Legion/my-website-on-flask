e3cd4b15443b5df2a8957e4ed2ef546ee5b6cd68
IT-Legion <it.s90x10@gmail.com>
2024-02-13 22:07:52 +0200

    Добавлено лого и стили к тексту домашней страницы.

--- app.py
+++ app.py
@@ -1,30 +1,59 @@
-from flask import Flask, render_template, url_for
+# Импорт класса Flask из библиотеки Flask и функции render_template для рендеринга HTML-шаблонов
+from flask import Flask, render_template  
+# Импорт класса SQLAlchemy для работы с базами данных SQLAlchemy в приложении Flask
+from flask_sqlalchemy import SQLAlchemy  
 
 
 
-app = Flask(__name__)
+# Создание экземпляра веб-приложения Flask с указанием текущего модуля в качестве имени
+app = Flask(__name__)  
+# Установка параметров конфигурации для подключения к базе данных SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  
+# Создание экземпляра SQLAlchemy и связывание его с приложением Flask
+db = SQLAlchemy(app)  
 
 
-@app.route('/')
-@app.route('/home')
-def index():
+# Определение модели данных Article для отображения в базе данных
+class Article(db.Model):  
+    # Поле id - первичный ключ таблицы
+    id = db.Column(db.Integer, primary_key=True)  
 
-    return render_template('index.html')
+    # Поле title - строка с максимальной длиной 100 символов, не может быть пустым
+    title = db.Column(db.String(100), nullable=False)  
+
+    # Поле content - текстовое поле, не может быть пустым
+    content = db.Column(db.Text, nullable=False)  
+
+    # Метод для строкового представления объекта Article
+    def __repr__(self):  
+        return f"Article(title='{self.title}', content='{self.content}')"
 
 
-@app.route('/about')
-def about():
 
+# Декоратор для установки маршрута '/' для вызова функции index
+@app.route('/')  
+# Декоратор для установки маршрута '/home' для вызова функции index
+@app.route('/home')  
+def index():  
+    # Функция для отображения главной страницы приложения
+    return render_template('index.html')  
+
+
+# Декоратор для установки маршрута '/about' для вызова функции about
+@app.route('/about')  
+def about():  
+    # Функция для отображения страницы "О нас"
     return render_template('about.html')
+  
+
+# Декоратор для установки маршрута '/user/<string:name>/<int:id>'
+@app.route('/user/<string:name>/<int:id>')  
+def user(name, id):  
+    # Функция для отображения страницы пользователя с указанным именем и идентификатором
+    return 'User page: ' + name + " - " + str(id)  
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return 'User page: ' + name + " - " + str(id)
-
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Условие для проверки, запущен ли этот скрипт напрямую
+if __name__ == '__main__':  
+    # Запуск веб-приложения Flask в режиме отладки
+    app.run(debug=True)  
